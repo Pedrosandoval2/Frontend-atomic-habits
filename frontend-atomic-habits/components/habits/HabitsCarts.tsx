9880d169ea521019b554d8ad83170cb9a1506d21
Pedrosandoval2 <pedrosandovalespinoza2@gmail.com>
2025-09-11 16:26:40 -0500

    feat: create habitscarts component and habitsttype component

---
+++ frontend-atomic-habits/components/habits/HabitsCarts.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const HabitsCarts = () => {
+    return (
+        <View>
+            <Text style={styles.text}>HabitsCarts</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: { color: 'white' }
+});
---
+++ frontend-atomic-habits/components/habits/HabitsType.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const HabitsType = () => {
+    return (
+        <View>
+            <Text style={styles.text}>HabitsType</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: { color: 'white' }
+});
